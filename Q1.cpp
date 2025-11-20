b743dbc282ff47379ee54aa9509c29dc333fd7e5
0-KESHAV-0 <keshav_y@mfs.iitr.ac.in>
2025-11-20 23:06:55 +0530

    skf

---
+++ Q1.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // code for hospital management system will go here using class //
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
-# This is for KESHAV
+#include <iostream>
+using namespace std;
+int main() {
+    int n; cin >>  n;
+    cout << n ;
+} 
